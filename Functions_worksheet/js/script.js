28904cbe6005d0956e80f0f60b42c9c0359ef1d5
mfinning <mfinning@gmail.com>
2014-03-27 18:00:51 -0600

    plying with  a life  calculator

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -1,9 +1,12 @@
-//Calculate the Area of a Rectangle
-
-
-function myFunction(a,b)
-{
-    return a*b;
+function calculateSupply(age, numPerDay) {
+    var maxAge = 100;
+    var totalNeeded = (numPerDay * 365) * (maxAge - age);
+    var message = 'You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge;
+    console.log(message);
 }
 
-document.getElementById("demo").innerHTML=myFunction(4,3);
+calculateSupply(28, 36);
+calculateSupply(28, 2.5);
+calculateSupply(28, 400);
+
+
