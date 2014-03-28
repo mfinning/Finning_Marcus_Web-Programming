4b704ea335f9fac8d89e8ecfdaa8e32b6ce4dc94
mfinning <mfinning@gmail.com>
2014-03-27 19:56:06 -0600

    removed an array

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,25 +1,15 @@
 //Finning_Marcus_function_wacky
 
-// fuctiob set up
-var a = function(){ console.log("Lets do this: shote") }
-var b = function(){ console.log("Better not: noshote") }
-var c = function(){ console.log("Not worth it : walkaway") }
-
-var foo = [a,b,c];
-
-while (foo.length){
-    foo.shift().call();
-}
-
+// fuction set up
 
 var array_of_functions = {
     "all": function(flag) {
         console.log(1+flag);
-    },
+    },//array calculation
     "cic": function(flag) {
-        console.log(13+flag);
+        console.log(15+flag);
     }
+    //array setup
 };
-
 array_of_functions.all(27);
-array_of_functions.cic(7);
+array_of_functions.cic(5);
