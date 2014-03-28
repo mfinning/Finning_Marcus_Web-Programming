985f1df6a9d72a4c5ededbecac6d63be662f8bfe
mfinning <mfinning@gmail.com>
2014-03-27 19:32:01 -0600

    noted  corrector an error that was not defining the dbl numbers

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,2 +1,26 @@
-//Finning
+//Finning_Marcus_function_industry
 
+//fuction set up
+
+
+var forEach = function (list, action) {
+    for (var i = 1; i < list.length; i++) {
+        action(list[i]);
+    }
+};
+//list it
+var map = function (mappingFunction, list) {
+    var result = [];
+    forEach(list, function (item) {
+        result.push(mappingFunction(item));
+    });
+    return result;
+};
+//time it with loop
+var doubleIt = function (item) {
+    if (typeof item === "number") {
+        return item * 2;
+    }
+};
+//print it
+console.log(map(doubleIt, [2,4, 6, 8, 20]));
